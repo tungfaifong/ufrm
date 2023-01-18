6cc53d46fd97561ae28bbfda2eecf7a752997b8e
tungfaifong <8497143+tungfaifong@user.noreply.gitee.com>
2023-01-18 15:18:34 +0800

    update scripts/common/net.lua.
    Signed-off-by: tungfaifong <8497143+tungfaifong@user.noreply.gitee.com>

---
+++ scripts/common/net.lua
@@ -0,0 +1,26 @@
+-- Copyright (c) 2022 TungFai Fong <iam@tungfaifong.com>
+
+-- 只有gamesrv可用
+function SendToClient(role_id, id, body)
+	net.SendToClient(role_id, id, {["body"]=body})
+end
+
+function SendToProxy(node_type, node_id, id, body, proxy_id, logic_type, msg_type, rpc_id)
+	proxy_id = proxy_id or 0
+	logic_type = logic_type or SSPkgHead.CPP
+	msg_type = msg_type or SSPkgHead.NORMAL
+	rpc_id = rpc_id or -1
+	net.SendToProxy(node_type, node_id, id, {["body"]=body}, proxy_id, logic_type, msg_type, rpc_id)
+end
+
+function BroadcastToProxy(node_type, id, body, proxy_id, logic_type)
+	proxy_id = proxy_id or 0
+	logic_type = logic_type or SSPkgHead.CPP
+	net.BroadcastToProxy(node_type, id, {["body"]=body}, proxy_id, logic_type)
+end
+
+function RpcProxy(coro_id, node_type, node_id, id, body, proxy_id)
+	proxy_id = proxy_id or 0
+	SendToProxy(node_type, node_id, id, body, proxy_id, SSPkgHead.LUA, SSPkgHead.RPCREQ, coro_id)
+	return coroutine.yield()
+end
