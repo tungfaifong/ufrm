0b77bf85e5e37cc9dc036371d2ba792db0b6f836
tungfaifong <iam@tungfaifong.com>
2023-01-18 15:18:12 +0800

    net.lua 默认值

--- scripts/gamesrv/common/net.lua
+++ scripts/gamesrv/common/net.lua
@@ -6,11 +6,17 @@ function SendToClient(role_id, id, body)
 end
 
 function SendToProxy(node_type, node_id, id, body, proxy_id, logic_type, msg_type, rpc_id)
+	proxy_id = proxy_id or 0
+	logic_type = logic_type or SSPkgHead.CPP
+	msg_type = msg_type or SSPkgHead.NORMAL
+	rpc_id = rpc_id or -1
 	local data, size = pblua.encode("SSPkgBody", body)
 	gamesrv.SendToProxy(node_type, node_id, id, data, size, proxy_id, logic_type, msg_type, rpc_id)
 end
 
 function BroadcastToProxy(node_type, id, body, proxy_id, logic_type)
+	proxy_id = proxy_id or 0
+	logic_type = logic_type or SSPkgHead.CPP
 	local data, size = pblua.encode("SSPkgBody", body)
 	gamesrv.BroadcastToProxy(node_type, id, data, size, proxy_id, logic_type)
 end
