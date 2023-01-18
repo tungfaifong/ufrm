f5853f00c1ae5b0acccba7c1ef44049261a3777b
tungfaifong <iam@tungfaifong.com>
2023-01-18 15:18:11 +0800

    优化

--- src/gateway/gateway.cpp
+++ src/gateway/gateway.cpp
@@ -15,8 +15,7 @@ Gateway::Gateway(NODEID id, toml::table & config) : _id(id), _config(config),
 	_lb_client(GATEWAY, _id, _config["Gateway"]["ip"].value_or(DEFAULT_IP), _config["Gateway"]["port"].value_or(DEFAULT_PORT), \
 	_config["LBSrv"]["id"].value_or(INVALID_NODE_ID), _config["LBSrv"]["ip"].value_or(DEFAULT_IP), _config["LBSrv"]["port"].value_or(DEFAULT_PORT), \
 	_config["LBSrv"]["timeout"].value_or(0)), 
-	_px_client(GATEWAY, _id, _config["Proxy"]["timeout"].value_or(0), _lb_client),
-	_db_client(_px_client)
+	_px_client(GATEWAY, _id, _config["Proxy"]["timeout"].value_or(0), _lb_client)
 {
 
 }
