10a78e32e72afba08c2f009c23d798776d66982c
tungfaifong <iam@tungfaifong.com>
2023-01-18 15:18:39 +0800

    优化client

--- test/client/main.cpp
+++ test/client/main.cpp
@@ -13,10 +13,12 @@
 
 using namespace usrv;
 
+class Client;
+
 uint64_t g_TotalDelay = 0;
 uint32_t g_TotalCnt = 0;
-uint32_t g_ClientNum = 0;
-
+int32_t g_ClientNum = 0;
+std::unordered_map<NETID, std::shared_ptr<Client>> g_Clients;
 
 class Client : public Unit, public std::enable_shared_from_this<Client>
 {
@@ -24,9 +26,7 @@ public:
 	Client(ROLEID role_id):_role_id(role_id) {}
 	~Client() = default;
 
-	virtual bool Init() override final;
 	virtual bool Start();
-	virtual bool Update(intvl_t interval);
 
 	void OnRecv(NETID net_id, char * data, uint16_t size);
 
@@ -42,49 +42,23 @@ public:
 	void OnHeartBeatRsp();
 
 private:
-	NETID _server_net_id = INVALID_NET_ID;
+	NETID _net_id = INVALID_NET_ID;
 	ROLEID _role_id;
 	std::chrono::steady_clock::time_point _start;
 	std::chrono::steady_clock::time_point _end;
-	std::shared_ptr<ServerUnit> _server;
 };
-
-bool Client::Init()
-{
-	auto server_key = "SERVER#"+std::to_string(_role_id);
-
-	_server = std::dynamic_pointer_cast<ServerUnit>(UnitManager::Instance()->Get(server_key.c_str()));
-
-	_server->OnConn([](NETID net_id, IP ip, PORT port){
-		LOGGER_INFO("conn: net_id:{} ip:{} port:{}", net_id, ip, port);
-	});
-
-	_server->OnRecv([self = shared_from_this()](NETID net_id, char * data, uint16_t size) {
-		self->OnRecv(net_id, data, size);
-	});
-
-	_server->OnDisc([](NETID net_id){
-		LOGGER_INFO("conn: net_id:{}", net_id);
-	});
-
-	return true;
-}
 
 bool Client::Start()
 {
-	_server_net_id = _server->Connect("127.0.0.1", 6666, 1000);
-	if(_server_net_id == INVALID_NET_ID)
+	_net_id = server::Connect("127.0.0.1", 6666, 1000);
+	if(_net_id == INVALID_NET_ID)
 	{
 		LOGGER_ERROR("client connect failed.");
 		return false;
 	}
+	g_Clients[_net_id] = shared_from_this();
 	AuthReq();
 	return true;
-}
-
-bool Client::Update(intvl_t interval)
-{
-	return false;
 }
 
 void Client::OnRecv(NETID net_id, char * data, uint16_t size)
@@ -129,7 +103,7 @@ void Client::SendToServer(CSID id, google::protobuf::Message * body)
 		LOGGER_ERROR("pkg size too long, id:{} size:{}", ENUM_NAME(id), size);
 		return;
 	}
-	_server->Send(_server_net_id, pkg.SerializeAsString().c_str(), (uint16_t)size);
+	server::Send(_net_id, pkg.SerializeAsString().c_str(), (uint16_t)size);
 	LOGGER_TRACE("send msg id:{}", ENUM_NAME(id));
 }
 
@@ -155,10 +129,7 @@ void Client::LoginReq()
 
 void Client::OnLoginRsp(const SCLoginRsp & rsp)
 {
-	for(auto i = 0; i < 1000; ++i)
-	{
-		HeartBeat();
-	}
+	HeartBeat();
 	LOGGER_INFO("OnLoginRsp:{} {}", _role_id, rsp.result());
 }
 
@@ -184,15 +155,33 @@ int main(int argc, char * argv[])
 	UnitManager::Instance()->Init(10);
 	UnitManager::Instance()->Register("LOGGER", std::move(std::make_shared<LoggerUnit>(LoggerUnit::LEVEL::INFO, "/logs/client.log", 1 Mi)));
 	UnitManager::Instance()->Register("TIMER", std::move(std::make_shared<TimerUnit>(1 Ki, 1 Ki)));
+	UnitManager::Instance()->Register("SERVER", std::move(std::make_shared<ServerUnit>(1 Ki, 1 Ki, 512 Ki)));
 	for(int i = 0; i < g_ClientNum; ++i)
 	{
-		auto server_key = "SERVER#"+std::to_string(i+1);
 		auto client_key = "CLIENT#"+std::to_string(i+1);
-		if(!UnitManager::Instance()->Register(server_key.c_str(), std::move(std::make_shared<ServerUnit>(512, 512, 1 Ki))) || !UnitManager::Instance()->Register(client_key.c_str(), std::move(std::make_shared<Client>(i+1))))
+		if(!UnitManager::Instance()->Register(client_key.c_str(), std::move(std::make_shared<Client>(i+1))))
 		{
-			LOGGER_ERROR("key:{} error", server_key);
+			LOGGER_ERROR("key:{} error", client_key);
 		}
 	}
+
+	auto server = std::dynamic_pointer_cast<ServerUnit>(UnitManager::Instance()->Get("SERVER"));
+
+	server->OnConn([](NETID net_id, IP ip, PORT port){
+		LOGGER_INFO("conn: net_id:{} ip:{} port:{}", net_id, ip, port);
+	});
+
+	server->OnRecv([&g_Clients](NETID net_id, char * data, uint16_t size) {
+		auto client = g_Clients.find(net_id);
+		if(client != g_Clients.end())
+		{
+			client->second->OnRecv(net_id, data, size);
+		}
+	});
+
+	server->OnDisc([](NETID net_id){
+		LOGGER_INFO("conn: net_id:{}", net_id);
+	});
 
 	timer::CreateTimer(time, [](){UnitManager::Instance()->SetExit(true);});
 
