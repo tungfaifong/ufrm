a58d4a0cbe68325b0dd70d36b34edb2c4932eb3b
tungfaifong <iam@tungfaifong.com>
2023-01-18 15:18:19 +0800

    更新iosrv

---
+++ scripts/iosrv/main.lua
@@ -0,0 +1,38 @@
+-- Copyright (c) 2022 TungFai Fong <iam@tungfaifong.com>
+
+package.path = package.path .. ';./scripts/?.lua;./scripts/iosrv/?.lua'
+
+require "common.util"
+
+function Start()
+	pblua.parse("common.proto")
+	pblua.parse("cs.proto")
+	pblua.parse("csid.proto")
+	pblua.parse("io.proto")
+	pblua.parse("ioid.proto")
+	pblua.parse("ss.proto")
+	pblua.parse("ssdcds.proto")
+	pblua.parse("ssgwgs.proto")
+	pblua.parse("ssid.proto")
+	pblua.parse("sslcls.proto")
+	pblua.parse("sspcpx.proto")
+	return true
+end
+
+function Update(interval)
+	return false
+end
+
+function Stop()
+end
+
+function OnConn(net_id, ip, port)
+end
+
+function OnRecv(net_id, data)
+	local pkg = pblua.decode("SSPkg", data)
+	logger.debug("Serialize:" .. Serialize(pkg))
+end
+
+function OnDisc(net_id)
+end
--- src/iosrv/main.cpp
+++ src/iosrv/main.cpp
@@ -5,6 +5,7 @@
 
 #include "toml++/toml.h"
 #include "usrv/units/logger_unit.h"
+#include "usrv/units/lua_unit.h"
 #include "usrv/units/server_unit.h"
 #include "usrv/units/timer_unit.h"
 #include "usrv/unit_manager.h"
@@ -48,6 +49,7 @@ int main(int argc, char * argv[])
 	UnitManager::Instance()->Register("SERVER", std::move(std::make_shared<ServerUnit>(config["Server"]["pp_alloc_num"].value_or(1 Ki), config["Server"]["ps_alloc_num"].value_or(1 Ki), config["Server"]["spsc_blk_num"].value_or(512 Ki))));
 	UnitManager::Instance()->Register("ISERVER", std::move(std::make_shared<ServerUnit>(config["IServer"]["pp_alloc_num"].value_or(1 Ki), config["IServer"]["ps_alloc_num"].value_or(1 Ki), config["IServer"]["spsc_blk_num"].value_or(512 Ki))));
 	UnitManager::Instance()->Register("TIMER", std::move(std::make_shared<TimerUnit>(config["Timer"]["tp_alloc_num"].value_or(1 Ki), config["Timer"]["ts_alloc_num"].value_or(1 Ki))));
+	// UnitManager::Instance()->Register("LUA", std::move(std::make_shared<LuaUnit>(config["Lua"]["path"].value_or(""), LuaExpose)));
 	UnitManager::Instance()->Register("IOSRV", std::move(std::make_shared<IOSrv>(config["IOSrv"]["id"].value_or(INVALID_NODE_ID), config)));
 
 	UnitManager::Instance()->Run();
