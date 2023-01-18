9abb6f1fda6b56b8c9b8abdb0a9214ab5d4cebb9
tungfaifong <8497143+tungfaifong@user.noreply.gitee.com>
2023-01-18 15:18:34 +0800

    更新tool

--- tools/start_all.sh
+++ tools/start_all.sh
@@ -4,7 +4,7 @@ cd .. && nohup ./bin/proxy /config/proxy.toml > /dev/null 2>&1 &
 sleep 1
 cd .. && nohup ./bin/dbsrv /config/dbsrv.toml > /dev/null 2>&1 & 
 sleep 1
-cd .. && nohup ./bin/iosrv /config/iosrv.toml > /dev/null 2>&1 & 
+cd .. && nohup ./bin/commonsrv /config/iosrv.toml > /dev/null 2>&1 & 
 sleep 1
 cd .. && nohup ./bin/gamesrv /config/gamesrv.toml > /dev/null 2>&1 & 
 sleep 1
